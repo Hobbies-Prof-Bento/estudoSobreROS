0e64d95ea9baf15e1caa59fa9fed67237678a90e
CLÍSTENES GRIZAFIS BENTO <68859813+Hobbies-Prof-Bento@users.noreply.github.com>
2021-07-15 19:32:44 -0300

    Create keyboard.py

---
+++ keyboard.py
@@ -0,0 +1,16 @@
+import sys, select
+import tty, termios
+
+settings = termios.tcgetattr(sys.stdin)
+
+def getKey():   
+
+    tty.setraw(sys.stdin.fileno())
+    rlist, _, _ = select.select([sys.stdin], [], [], 0.1)
+    if rlist:
+        key = sys.stdin.read(1)
+    else:
+        key = ''
+
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+    return key
